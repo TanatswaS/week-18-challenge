a0eca3997a4488c5b41eb86f7db82b4464e0c69d
Mister Tanatswa <tanatswasingano@gmail.com>
2022-08-05 16:46:24 -0400

    implemented thought controller and added api routes

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,23 +1,27 @@
 // Require Users Model
-const {Users} = require('../models');
+const { Users } = require('../models');
 
 // Set up Users Controller
 const usersController = {
-
     // Create a new User
     createUsers({body}, res) {
         Users.create(body)
             .then(dbUsersData => res.json(dbUsersData))
             .catch(err => res.status(400).json(err));
     },
-
     // Get All Users
     getAllUsers(req, res) {
         Users.find({})
         // populate users thoughts
-            .populate({path: 'thoughts', select: '-__v'})
+            .populate({
+                path: 'thoughts',
+                select: '-__v'
+            })
             // populate user friends
-            .populate({path: 'friends', select: '-__v'})
+            .populate({
+                path: 'friends',
+                select: '-__v'
+            })
             .select('-__v')
             // .sort({_id: -1})
             .then(dbUsersData => res.json(dbUsersData))
@@ -26,12 +30,17 @@ const usersController = {
                 res.status(500).json(err);
             });
     },
-
     // Get single user by ID
     getUsersById({params}, res) {
         Users.findOne({_id: params.id })
-            .populate({path: 'thoughts', select: '-__v'})
-            .populate({path: 'friends', select: '-__v'})
+            .populate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            .populate({
+                path: 'friends',
+                select: '-__v'
+            })
             .select('-__v')
             // return if no user is found
             .then(dbUsersData => {
@@ -46,7 +55,6 @@ const usersController = {
                 res.status(400).json(err)
             })
     },
-
     // Update a current User by ID
     updateUsers({params, body}, res) {
         Users.findOneAndUpdate({_id: params.id}, body, {new: true, runValidators: true})
@@ -59,7 +67,6 @@ const usersController = {
             })
             .catch(err => res.json(err))
     },
-
     deleteUsers({params}, res) {
         Users.findOneAndDelete({_id: params.id})
             .then(dbUsersData => {
@@ -71,11 +78,13 @@ const usersController = {
             })
             .catch(err => res.status(400).json(err));
     },
-
     // Delete a current user by ID
     addFriend({params}, res) {
         Users.findOneAndUpdate({_id: params.id}, {$push: { friends: params.friendId}}, {new: true})
-            .populate({path: 'friends', select: ('-__v')})
+            .populate({
+                path: 'friends',
+                select: '-__v'
+            })
             .select('-__v')
             .then(dbUsersData => {
                 if (!dbUsersData) {
@@ -86,11 +95,13 @@ const usersController = {
             })
             .catch(err => res.json(err));
     },
-
     // Delete a current Friend
     deleteFriend({ params }, res) {
         Users.findOneAndUpdate({_id: params.id}, {$pull: { friends: params.friendId}}, {new: true})
-            .populate({path: 'friends', select: '-__v'})
+            .populate({
+                path: 'friends',
+                select: '-__v'
+            })
             .select('-__v')
             .then(dbUsersData => {
                 if(!dbUsersData) {
@@ -101,8 +112,6 @@ const usersController = {
             })
             .catch(err => res.status(400).json(err));
     }
-
 };
-
 // Export module users controller
 module.exports = usersController;
